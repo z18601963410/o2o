7f615c373d845693a0ffabdabd97e4bda0afd31e
100012432483 <644511451@qq.com>
2021-05-18 13:53:23 +0800

    输入 域名/o2o/ 默认重定向到首页 frontend/index.html

--- src/main/java/com/ike/o2o/Hello.java
+++ src/main/java/com/ike/o2o/Hello.java
@@ -1,13 +1,24 @@
 package com.ike.o2o;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //等效于@RequestMapping + @ResponseBody
+//@RestController //等效于@RequestMapping + @ResponseBody  向前端直接输出内容
+@Controller
 public class Hello {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
         return "Hello SpringBoot!";
+    }
+
+    /**
+     * 输入/o2o 默认重定向到首页
+     * @return
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    private String forwardToFrontendIndex() {
+        return "redirect:/frontend/index";
     }
 }
